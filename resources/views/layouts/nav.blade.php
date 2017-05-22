1c31be8991d5fe524cf9e0ec3f42eb9100ede924
amitoshseu <amitosh_cse_seu@yahoo.com>
2017-05-23 01:25:42 +0600

    profile nav bar added

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,13 +1,35 @@
 <div class="blog-masthead">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item" href="/">Home</a>
           <a class="blog-nav-item" href="#">New features</a>
           <a class="blog-nav-item" href="#">Press</a>
           <a class="blog-nav-item" href="#">New hires</a>
-          @if(Auth::check())
-            <a class="blog-nav-item  ml-auto" href="#">{{Auth::user()->name}}</a>
-          @endif
+          
+
+        <div class="dropdown pull-right">
+            <a class="blog-nav-item" href="#" data-toggle="dropdown">Profile
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li>
+                @if(Auth::check())
+                  <a class="blog-nav-item " href="#">{{Auth::user()->name}}</a>
+                  <li><a class="blog-nav-item" href="/logout">Logout</a></li>
+                @else
+                  <li><a class="blog-nav-item" href="/login">Login</a></li>
+                  <li><a class="blog-nav-item" href="/register">Register</a></li>
+                @endif
+              </li>
+
+              
+                @if(Auth::check())
+                    <li class="divider"></li>
+                    <li><a class="blog-nav-item" href="/posts/create">Create Post</a></li>
+                @endif
+
+            </ul>
+        </div>
+
         </nav>
       </div>
   </div>
